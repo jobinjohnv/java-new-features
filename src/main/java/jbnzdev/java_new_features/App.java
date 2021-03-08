efea765a562b19fb21b777fc0ce98c9d5e3a4286
Jobin John V <Jobin.JohnV@arm.com>
2021-03-08 23:32:33 +0000

    Initial Commit

---
+++ src/main/java/jbnzdev/java_new_features/App.java
@@ -0,0 +1,13 @@
+package jbnzdev.java_new_features;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
